410c86bfc8c3c1a18a5215094a7fd8ea2ba1494d
Meghna <66563032+Magii204@users.noreply.github.com>
2023-11-11 17:49:29 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+ReactDom.render(
+  <div>
+    <h1>My favourite food</h1>
+    <ul>
+      <li>apple</li>
+      <li>banana</li>
+      <li>custardapple</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
